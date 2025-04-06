7d65ef2f4529738ff650b7de38d5572a9f460bb3
cxmi <christinexmi@gmail.com>
2025-04-06 16:55:05 -0400

    created active UI manager

---
+++ Incremental-Game/Assets/Scripts/ActiveUIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActiveUIManager : MonoBehaviour
+{
+    public Button startReligion;
+    public GameObject worshipperStats;
+    public GameObject goldStats;
+    public GameObject worshipperPanel;
+    public GameObject convertPrefab;
+    public GameObject armPrefab;
+    public GameObject crucifyPrefab;
+    public GameObject goldPanel;
+    public GameObject tithePrefab;
+    public GameObject indulgencesPrefab;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
