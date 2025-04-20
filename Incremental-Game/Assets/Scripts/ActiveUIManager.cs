fbd5ec08844a5aba17034a23a64d684988ad62d5
cxmi <christinexmi@gmail.com>
2025-04-20 13:59:42 -0400

    active UI updates

--- Incremental-Game/Assets/Scripts/ActiveUIManager.cs
+++ Incremental-Game/Assets/Scripts/ActiveUIManager.cs
@@ -8,7 +8,7 @@ public class ActiveUIManager : MonoBehaviour
     
     public GameObject startReligion;
     [HideInInspector] public GameObject worshipperStats;
-    [HideInInspector] public GameObject goldStats;
+    public GameObject goldStats;
     [HideInInspector] public GameObject worshipperPanel;
     [HideInInspector] public GameObject convertPrefab;
     [HideInInspector] public GameObject armPrefab;
@@ -18,6 +18,13 @@ public class ActiveUIManager : MonoBehaviour
     public GameObject megachurchPrefab;
     public GameObject tithePrefab;
     public GameObject indulgencePrefab;
+    public GameObject taxPrefab;
+    public GameObject extortPrefab;
+    public GameObject annexPrefab;
+    public GameObject crusadePrefab;
+    public GameObject holyWarPrefab;
+    public GameObject executePrefab;
+    public GameObject martyrPrefab;
     public GameObject goldPanel;
     //public GameObject tithePrefab;
     //public GameObject indulgencesPrefab;
@@ -31,10 +38,26 @@ public class ActiveUIManager : MonoBehaviour
     private Button armButton;
     private Button crucifyButton;
     private Button megachurchButton;
+    private Button titheButton;
+    private Button indulgenceButton;
+    private Button taxButton;
+    private Button extortButton;
+    private Button annexButton;
+    private Button crusadeButton;
+    private Button executeButton;
+    private Button martyrButton;
     
     private bool seenArm = false;
     private bool seenCrucify = false;
     private bool seenMegachurch = false;
+    private bool seenTithe = false;
+    private bool seenTax = false;
+    private bool seenExtort = false;
+    private bool seenAnnex = false;
+    private bool seenCrusade = false;
+    private bool startHolyWar = false;
+    private bool seenExecute = false;
+    private bool seenMartyr = false;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -50,6 +73,22 @@ public class ActiveUIManager : MonoBehaviour
         armButton = armPrefab.GetComponentInChildren<Button>();
         crucifyButton = crucifyPrefab.GetComponentInChildren<Button>();
         megachurchButton = megachurchPrefab.GetComponentInChildren<Button>();
+        titheButton = tithePrefab.GetComponentInChildren<Button>();
+        indulgenceButton = indulgencePrefab.GetComponentInChildren<Button>();
+        taxButton = taxPrefab.GetComponentInChildren<Button>();
+        extortButton = extortPrefab.GetComponentInChildren<Button>();
+        annexButton = annexPrefab.GetComponentInChildren<Button>();
+        crusadeButton = crusadePrefab.GetComponentInChildren<Button>();
+        executeButton = executePrefab.GetComponentInChildren<Button>();
+        martyrButton = martyrPrefab.GetComponentInChildren<Button>();
+        
+        //deactivate some prefabs ahead of time
+        indulgencePrefab.SetActive(false);
+        taxPrefab.SetActive(false);
+        extortPrefab.SetActive(false);
+        crusadePrefab.SetActive(false);
+        executePrefab.SetActive(false);
+        martyrPrefab.SetActive(false);
 
         
     }
@@ -71,8 +110,7 @@ public class ActiveUIManager : MonoBehaviour
             }
 
             //HIDE panels until religion is founded
-            //TODO: uncomment this once ready to go live
-            //
+       
 
             if (!religionFounded)
             {
@@ -89,10 +127,11 @@ public class ActiveUIManager : MonoBehaviour
                 worshipperStats.SetActive(true);
                 worshipperPanel.SetActive(true);
             }
-
-            //end TODO
+            
 
             //------------GATING LOGIC-----------
+            //-----------Ensures you have at least 1 of something before the next item appears--------
+            
             if (gm.numberOwned[(int)GameManager.InvestmentType.Convert] > 1 || seenArm)
             {
                 armPrefab.SetActive(true);
@@ -116,22 +155,53 @@ public class ActiveUIManager : MonoBehaviour
                 crucifyPrefab.SetActive(false);
             }
 
-            //show megachurch and reveal gold panel
+            //show megachurch and reveal gold panel after buying Crucify
             if (gm.numberOwned[(int)GameManager.InvestmentType.Crucify] > 0
                 || gm.karma >= gm.megachurchCost
                 || seenMegachurch)
             {
                 megachurchPrefab.SetActive(true);
                 seenMegachurch = true;
+                seenTithe = true;
                 goldPanel.SetActive(true);
+                goldStats.SetActive(true);
             }
             else
             {
                 megachurchPrefab.SetActive(false);
-                //uncomment below when NOT testing
-
-                //goldPanel.SetActive(false);
+                goldPanel.SetActive(false);
             }
+            
+            //show indulgences and taxes after buying Tithe
+            if (gm.numberOwned[(int)GameManager.InvestmentType.Tithe] > 0
+                || seenTax)
+            {
+                indulgencePrefab.SetActive(true);
+                taxPrefab.SetActive(true);
+                seenTax = true;
+            }
+            else
+            {
+                indulgencePrefab.SetActive(false);
+                taxPrefab.SetActive(false);
+            }
+            
+            //show extort after buying tax or indulgence
+            if (gm.numberOwned[(int)GameManager.InvestmentType.Tax] > 0
+                || gm.numberOwned[(int)GameManager.InvestmentType.Indulgence] > 0
+                || seenExtort)
+            {
+                extortPrefab.SetActive(true);
+                seenExtort = true;
+            }
+            else
+            {
+                extortPrefab.SetActive(false);
+            }
+            
+            
+            
+        
 
 
 
@@ -139,6 +209,7 @@ public class ActiveUIManager : MonoBehaviour
 
 
             //----------BUTTON ACTIVATION LOGIC----------
+            //---------Checks if you have enough karma or gold to buy------------
 
             //convert
             if (gm.karma < gm.convertCost)
@@ -172,13 +243,100 @@ public class ActiveUIManager : MonoBehaviour
 
 
             //megachurch
-            if (gm.karma < gm.megachurchCost || gm.gold < gm.megachurchCost)
+            if (gm.karma < gm.megachurchCost || gm.gold < gm.megachurchGold)
             {
                 megachurchButton.interactable = false;
             }
             else
             {
                 megachurchButton.interactable = true;
+            }
+            
+            // tithe
+            if (gm.karma < gm.titheCost)
+            {
+                titheButton.interactable = false;
+            }
+            else
+            {
+                titheButton.interactable = true;
+            }
+            
+            //indulgence
+
+            if (gm.karma < gm.indulgenceCost)
+            {
+                indulgenceButton.interactable = false;
+            }
+            else
+            {
+                indulgenceButton.interactable = true;
+            }
+            
+            //tax
+
+            if (gm.karma < gm.taxCost)
+            {
+                taxButton.interactable = false;
+            }
+            else
+            {
+                taxButton.interactable = true;
+            }
+            
+            //extort
+
+            if (gm.karma < gm.extortCost)
+            {
+                extortButton.interactable = false;
+            }
+            else
+            {
+                extortButton.interactable = true;
+            }
+            
+            //annex
+
+            if (gm.karma < gm.annexCost || gm.gold < gm.annexGoldCost) 
+            {
+                annexButton.interactable = false;
+            }
+            else
+            {
+                annexButton.interactable = true;
+            }
+            
+            //crusade
+
+            if (gm.karma < gm.crusadeCost || gm.gold < gm.crusadeGoldCost)
+            {
+                crusadeButton.interactable = false;
+            }
+            else
+            {
+                crusadeButton.interactable = true;
+            }
+            
+            //execute
+
+            if (gm.karma < gm.executeCost)
+            {
+                executeButton.interactable = false;
+            }
+            else
+            {
+                executeButton.interactable = true;
+            }
+            
+            //martyr
+
+            if (gm.karma < gm.martyrCost)
+            {
+                martyrButton.interactable = false;
+            }
+            else
+            {
+                martyrButton.interactable = true;
             }
 
 
